1006f7b635204f37fcc4061892e472275babee37
Isabella Aguiar <isabella.aguiar@capgemini.com>
2026-03-12 16:47:12 -0300

    dia 04 - Who will pay the bill

--- Day 4/Banker Roulette/task.py
+++ Day 4/Banker Roulette/task.py
@@ -1,1 +1,11 @@
+import random
+
+
+# 1st option
 friends = ["Alice", "Bob", "Charlie", "David", "Emanuel"]
+random_friends = random.choice(friends)
+print(random_friends)
+
+# 2nd option
+randon_index = random.randint(0, 4)
+print(friends[randon_index])
