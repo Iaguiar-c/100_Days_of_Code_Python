bd11b1228713b511864371dd6cfc52e7bef34a37
Isabella Aguiar <isabella.aguiar@capgemini.com>
2026-03-12 15:51:35 -0300

    dia 04 - Understanding the Offset and Appendind Items to Lists

---
+++ Day 4/Lists/task.py
@@ -0,0 +1,15 @@
+states_of_america = ["Delaware", "Pennsylvania", "New Jersey"]
+# print(states_of_america[0])
+
+# ALTERAR UM ITEM DA LISTA
+states_of_america[1] = "Georgia"
+# print(states_of_america)
+
+# ADICIONAR À LISTA
+states_of_america.append("Rhode Island")
+# print(states_of_america)
+
+# ADICIONAR VÁRIOS ITENS AO FINAL DA LISTA
+states_of_america.extend(["California", "New York"])
+print(states_of_america)
+
