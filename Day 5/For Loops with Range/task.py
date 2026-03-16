dfbceb399264e909b3411beddffb2d6222be26c9
Isabella Aguiar <isabella.aguiar@capgemini.com>
2026-03-16 15:06:39 -0300

    dia 05 - for loops and the range() function

---
+++ Day 5/For Loops with Range/task.py
@@ -0,0 +1,18 @@
+# Range - intervalo de números
+# for number in range(a, b - b não incluso):
+#     print number
+
+# Range in For Loop
+# for number in range(1, 10):
+#     print(number)
+
+# Range pulando numeros, adiciona mais uma vírgula e coloca de quanto em quanto quer pular
+# for number in range(1, 11, 3):
+#     print(number)
+
+# PAUSE 1
+sum = 0
+for number in range(1,101):
+    sum += number
+
+print(sum)
