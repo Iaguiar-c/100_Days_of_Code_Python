a7796726c64d5813857c53e3db6cdeaab99c4311
Isabella Aguiar <isabella.aguiar@capgemini.com>
2026-03-17 13:35:15 -0300

    dia 05 - Project: Create a Password Generator

--- Day 5/Password Generator Project/task.py
+++ Day 5/Password Generator Project/task.py
@@ -1,3 +1,5 @@
+import random
+
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
@@ -8,3 +10,43 @@ nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
 
+# easy level
+# password = ""
+#
+# # range de 1 ate a quantidade de caracteres que o usuario quer
+# # exemplo: ele quer 4 letras então char vai percorrer até o 3, pois , não inclui o 4, ou seja, seria apenas abc
+# # poderia fazer range(1, nr_letters + 1)
+# # o +1 é para incluir o exato que o usuario quer, ser excluir o ultimo, então agora sim, são exatamente 4 sem excluir, ficando abcd
+# for char in range(0, nr_letters):
+#     password += random.choice(letters)
+#
+# for char in range(0, nr_symbols):
+#     password += random.choice(symbols)
+#
+# for char in range(0, nr_numbers):
+#     password += random.choice(numbers)
+#
+# print(password)
+
+# hard level
+password_list = []
+
+for char in range(0, nr_letters):
+    password_list.append(random.choice(letters))
+
+for char in range(0, nr_symbols):
+    password_list.append(random.choice(symbols))
+
+for char in range(0, nr_numbers):
+    password_list.append(random.choice(numbers))
+
+# print(password_list)
+random.shuffle(password_list)
+# print(password_list)
+
+password = ""
+
+for char in password_list:
+    password += char
+
+print(f"Your password is: {password}")
