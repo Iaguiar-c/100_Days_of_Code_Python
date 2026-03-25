590c0147d6aa5cad7684f94a9704edc4b42533f9
Isabella Aguiar <bellacorreia2004@gmail.com>
2026-03-25 11:25:28 -0300

    dia 08 - Positional vs Keyword Arguments

--- Day 8/Positional vs Keyword Arguments/task.py
+++ Day 8/Positional vs Keyword Arguments/task.py
@@ -1,8 +1,22 @@
 # Functions with input
 
-def greet_with_name(name):
+# def greet_with_name(name):
+#     print(f"Hello {name}")
+#     print(f"How do you do {name}?")
+#
+#
+# greet_with_name("Jack Bauer")
+
+# # Functions with more than 1 input
+# def greet_with(name, location):
+#     print(f"Hello {name}")
+#     print(f"What is it like in {location}")
+#
+# greet_with("Bella", "Praia Grande")
+
+# Functions with keyword
+def greet_with(name, location):
     print(f"Hello {name}")
-    print(f"How do you do {name}?")
+    print(f"What is it like in {location}")
 
-
-greet_with_name("Jack Bauer")
+greet_with(location = "Praia Grande", name = "Bella")
