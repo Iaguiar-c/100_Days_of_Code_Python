7bfa6f8970a523905b12de1c97fb82454ba5d1a1
Isabella Aguiar <bellacorreia2004@gmail.com>
2026-03-25 09:55:05 -0300

    dia 08 - Functions with inputs

---
+++ Day 8/Functions with Inputs/task.py
@@ -0,0 +1,14 @@
+# PAUSE 1
+def greet():
+    print("The sky is blue")
+    print("The grass is green")
+    print("My love for you had never fim.")
+
+greet()
+
+# Functions that allows inputs
+def greet_with_name(name):
+    print(f"Hello {name}")
+    print(f"How do you do {name}")
+
+greet_with_name("Bella")
