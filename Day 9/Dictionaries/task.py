78767c14188de576a2d18eab2f2ebfca3ca7fef8
Isabella Aguiar <bellacorreia2004@gmail.com>
2026-03-26 09:35:53 -0300

    dia 09 - The Python Dictionary: Deep Dive

--- Day 9/Dictionaries/task.py
+++ Day 9/Dictionaries/task.py
@@ -1,1 +1,21 @@
-programming_dictionary = {"Bug": "An error in a program that prevents the program from running as expected.", "Function": "A piece of code that you can easily call over and over again."}
+programming_dictionary = {
+    "Bug": "An error in a program that prevents the program from running as expected.",
+    "Function": "A piece of code that you can easily call over and over again."
+}
+
+# print(programming_dictionary["Bug"])
+
+# Add values
+programming_dictionary["Loop"] = "The action..."
+# print(programming_dictionary)
+
+empty_dictionary = {}
+# ele tambem limpa um dicionario que ja existe se executar
+
+# editar
+programming_dictionary["Bug"] = "Bla"
+
+# loop
+for key in programming_dictionary:
+    print(key) #ele retorna aqui apenas a chave
+    print(programming_dictionary[key]) # retona apenas o valor dos valores das chaves
