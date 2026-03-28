a1cdbdcda39c18f1f6d5399a16af52510ba66e9e
Isabella Aguiar <bellacorreia2004@gmail.com>
2026-03-27 22:04:53 -0300

    dia 10 - Docstrings

--- Day 10/Docstrings/task.py
+++ Day 10/Docstrings/task.py
@@ -1,4 +1,5 @@
 def format_name(f_name, l_name):
+    """Escreve a documentação aqui"""
     formated_f_name = f_name.title()
     formated_l_name = l_name.title()
     return f"{formated_f_name} {formated_l_name}"
@@ -7,6 +8,10 @@ def format_name(f_name, l_name):
 formatted_name = format_name("AnGeLa", "YU")
 
 length = len(formatted_name)
+
+"""
+assim tambem
+"""
 
 
 
