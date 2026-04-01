5712a7284377a5ca5785a6c9bf13ee1e24814c35
Isabella Aguiar <bellacorreia2004@gmail.com>
2026-04-01 09:00:20 -0300

    dia 13 - Describe the Problem

--- Day 13/Describe the Problem/task.py
+++ Day 13/Describe the Problem/task.py
@@ -1,5 +1,5 @@
 def my_function():
-    for i in range(1, 20):
+    for i in range(1, 21):
         if i == 20:
             print("You got it")
 
@@ -8,5 +8,8 @@ my_function()
 
 # Describe the Problem - Write your answers as comments:
 # 1. What is the for loop doing?
+# the for loop is running i in 1 to 20 - 19, so is 1 to 19 verifing if i is equal to 20
 # 2. When is the function meant to print "You got it"?
+# only if i is 20
 # 3. What are your assumptions about the value of i?
+# i is gona assume each range value that goes 1 to 19
